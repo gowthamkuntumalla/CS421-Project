4b9cccb2ae31696cfbbeb050b59c60fcafce5ef7
Gowtham Kuntumalla <gowthamkuntumalla@gmail.com>
2021-08-02 23:13:34 -0500

    add

--- fractals/src/Lib.hs
+++ fractals/src/Lib.hs
@@ -1,6 +1,26 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+type Point = (Float, Float)
+
+next :: Point -> Point -> Point
+next (u,v) (x,y) = (x*x-y*y+u,2*x*y +v)
+
+mandelbrot :: Point -> [Point] 
+mandelbrot p = iterate (next p) (0, 0)
+
+fairlyClose :: Point -> Bool
+fairlyClose (u,v) = (u*u+v*v)<100
+
+
+inMandelbrotSet :: Point -> Bool 
+inMandelbrotSet p = all fairlyClose (mandelbrot p)
+
+approxTest :: Int -> Point -> Bool
+approxTest n p = all fairlyClose (take n (mandelbrot p))
+
+
+chooseColor :: [color] -> [Point] -> color
+chooseColor palette = (palette !!) . length . take n . takeWhile fairlyClose where n = length palette - 1
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "someFuncstring"
